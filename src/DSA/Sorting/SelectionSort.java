d6503c613a497bef1fee98a70fbd1823a9fbe038
iamkhusro <zaheenkhusro@gmail.com>
2025-01-09 00:40:42 +0530

    Sorting: Selection

--- src/DSA/Sorting/SelectionSort.java
+++ src/DSA/Sorting/SelectionSort.java
@@ -2,20 +2,23 @@ package DSA.Sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+
+public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5,7,15,9,1,3,2,11,13};
         int size = arr.length;
         System.out.println("Before sorting: " + Arrays.toString(arr));
 
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
 
             //to check intermediate data
             System.out.println(Arrays.toString(arr));
