1b319c3915ac2b43e66283162ad9ceb7fb607d97
iamkhusro <zaheenkhusro@gmail.com>
2025-01-09 14:09:02 +0530

    Sorting: Insertion

--- src/DSA/Sorting/InsertionSort.java
+++ src/DSA/Sorting/InsertionSort.java
@@ -2,23 +2,22 @@ package DSA.Sorting;
 
 import java.util.Arrays;
 
-
-public class SelectionSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {5,7,15,9,1,3,2,11,13};
         int size = arr.length;
         System.out.println("Before sorting: " + Arrays.toString(arr));
 
-        for (int i = 0; i < size - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-                if(arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
+        for (int i = 1; i < size; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+
+            arr[j + 1] = key;
 
             //to check intermediate data
             System.out.println(Arrays.toString(arr));
